f5760e38db3f0a3e94e66be26c0d22c778ba165c
ngtrungson <sonngtrung@gmail.com>
2018-08-03 16:50:31 +0700

    update data 03.08

--- VNINDEX.py
+++ VNINDEX.py
@@ -401,9 +401,9 @@ def rebalancing_porfolio(symbols = None, bench = '^VNINDEX'):
 
     
 if __name__ == "__main__":
-#    import sys
-#    old_stdout = sys.stdout
-#    sys.stdout=open("logging.txt","w")
+    import sys
+    old_stdout = sys.stdout
+    sys.stdout=open("logging.txt","w")
 ##   
 #    
 #    symbols = get_csv_data(source = "cp68")
@@ -419,9 +419,9 @@ if __name__ == "__main__":
 #    VNI_result, VNI_data  = passive_strategy(start_date = "2017-3-26" , end_date = "2018-4-24", market= "^VNINDEX")
     
 
-    ticker = 'SHB'    
+    ticker = 'GEX'    
 #
-    end_date = "2018-7-31"
+    end_date = "2018-8-3"
     start_date = "2017-1-2"
 #####    bollingerbands = bollinger_bands(ticker, start_date, end_date, realtime = False, source = "cp68")
 ####    
@@ -440,7 +440,7 @@ if __name__ == "__main__":
 ##    
 ##    investment_stocks = ['CII', 'HPG', 'NBB', 'STB', 'PAN', 'VND' ]
 ##    
-#    canslim = hung_canslim(ticker, start_date, end_date, realtime = False,  source ="cp68", ndays = 30, typetrade = 'Bottom') 
+#    canslim = hung_canslim(ticker, start_date, end_date, realtime = True,  source ="cp68", ndays = 10, typetrade = 'Long') 
 #    meanrevert = mean_reversion(ticker, start_date, end_date, realtime = False,  source ="cp68") 
 ###    plot_canslim_trading(ticker, canslim)
 
@@ -449,7 +449,7 @@ if __name__ == "__main__":
 #               'BVH', 'TCH', 'PMG',  'VJC', 'GEX', 'MSN',
 #              'DGW',    'PNJ',  'PAN', 'GAS', 'DXG', 'IDI', 'VIC', 'ANV',
 #              'MSR', 'MCH', 'TVB', 'TBD']
-    analysis_trading(tickers = None, start = "2017-1-2" , end = "2018-8-1", update = False,  source ="cp68")
+    analysis_trading(tickers = None, start = "2017-1-2" , end = "2018-8-3", update = False,  source ="cp68")
 #    
 #    
 #    stock_all = analysis_stocks(start_date = start_date, end_date = end_date)
@@ -473,5 +473,5 @@ if __name__ == "__main__":
 #    for ticker in RSWlist:
 #        ML_strategy(ticker, start ="2011-1-2", end = "2018-5-24")
 #    tickers = pd.Series(symbols)
-#    sys.stdout = old_stdout
+    sys.stdout = old_stdout
     
