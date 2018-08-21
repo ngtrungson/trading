9f6e6f6b19ecf145ee479af96842c1124db8ac69
ngtrungson <36395701+ngtrungson@users.noreply.github.com>
2018-08-21 10:53:53 +0700

    update database 20.08

--- VNINDEX.py
+++ VNINDEX.py
@@ -421,8 +421,8 @@ if __name__ == "__main__":
 
     ticker = 'GEX'    
 #
-    end_date = "2018-8-3"
-    start_date = "2017-1-2"
+    end_date = "2018-8-10"
+    start_date = "2018-3-1"
 #####    bollingerbands = bollinger_bands(ticker, start_date, end_date, realtime = False, source = "cp68")
 ####    
 #    hedgefund = hedgefund_trading(ticker, start_date, end_date, realtime = False, source ="cp68")    
@@ -449,7 +449,7 @@ if __name__ == "__main__":
 #               'BVH', 'TCH', 'PMG',  'VJC', 'GEX', 'MSN',
 #              'DGW',    'PNJ',  'PAN', 'GAS', 'DXG', 'IDI', 'VIC', 'ANV',
 #              'MSR', 'MCH', 'TVB', 'TBD']
-    analysis_trading(tickers = None, start = "2017-1-2" , end = "2018-8-8", update = False,  source ="cp68")
+#    analysis_trading(tickers = None, start = "2017-1-2" , end = "2018-8-20", update = False,  source ="cp68")
 #    
 #    
 #    stock_all = analysis_stocks(start_date = start_date, end_date = end_date)
