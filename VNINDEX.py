1514548466fa0072849e18e3838c0ecdc5b197a1
ngtrungson <sonngtrung@gmail.com>
2018-09-26 16:02:06 +0700

    update database 26.9

--- VNINDEX.py
+++ VNINDEX.py
@@ -450,7 +450,7 @@ if __name__ == "__main__":
 #               'BVH', 'TCH', 'PMG',  'VJC', 'GEX', 'MSN',
 #              'DGW',    'PNJ',  'PAN', 'GAS', 'DXG', 'IDI', 'VIC', 'ANV',
 #              'MSR', 'MCH', 'TVB', 'TBD']
-    analysis_trading(tickers = None, start = "2017-1-2" , end = "2018-9-25", update = False,  source ="cp68")
+    analysis_trading(tickers = None, start = "2017-1-2" , end = "2018-9-26", update = False,  source ="cp68")
 #    
 #    
 
