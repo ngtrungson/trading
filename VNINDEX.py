f5091a3c4bf7424f06badad5784946b3aee795f4
ngtrungson <sonngtrung@gmail.com>
2018-10-04 15:59:33 +0700

    update database 04.10

--- VNINDEX.py
+++ VNINDEX.py
@@ -425,7 +425,7 @@ if __name__ == "__main__":
 
     ticker = 'GEX'    
 #
-    end_date = "2018-10-2"
+    end_date = "2018-10-4"
     start_date = "2018-4-10"
 #####    bollingerbands = bollinger_bands(ticker, start_date, end_date, realtime = False, source = "cp68")
 ####    
@@ -453,11 +453,11 @@ if __name__ == "__main__":
 #               'BVH', 'TCH', 'PMG',  'VJC', 'GEX', 'MSN',
 #              'DGW',    'PNJ',  'PAN', 'GAS', 'DXG', 'IDI', 'VIC', 'ANV',
 #              'MSR', 'MCH', 'TVB', 'TBD']
-    analysis_trading(tickers = None, start = "2017-1-2" , end = "2018-10-3", update = False,  source ="cp68")
+#    analysis_trading(tickers = None, start = "2017-1-2" , end = "2018-10-3", update = True,  source ="cp68")
 #    
 #    
 
-#    stock_all = analysis_stocks(start_date = start_date, end_date = end_date)
+    stock_all = analysis_stocks(start_date = start_date, end_date = end_date)
 #    
     
 #    symbolsVNI = getliststocks(typestock = "^VNINDEX")
