eee2ff2ace0e42473137c47dc3ac71902ef12af8
ngtrungson <36395701+ngtrungson@users.noreply.github.com>
2018-11-12 22:52:17 +0700

    update database 12.11

--- VNINDEX.py
+++ VNINDEX.py
@@ -558,7 +558,7 @@ if __name__ == "__main__":
 
     ticker = 'GEX'    
 #
-    end_date = "2018-11-9"
+    end_date = "2018-11-12"
     start_date = "2018-4-6"
 #####    bollingerbands = bollinger_bands(ticker, start_date, end_date, realtime = False, source = "cp68")
 ####    
