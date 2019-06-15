7aa74cf3234f7d0fe487388abf6151f54830f730
ngtrungson <sonngtrung@gmail.com>
2019-06-15 08:58:44 +0700

    update database 14.06

--- VNINDEX.py
+++ VNINDEX.py
@@ -554,7 +554,7 @@ if __name__ == "__main__":
     sys.stdout=open("logging.txt","w")
 #   
 #    
-#    symbols = get_csv_data(source = "ssi")
+#    symbols = get_csv_data(source = "cp68")
 #    symbols = get_csv_data()
 #    symbols = get_stocks_highcpm(download = False, source ="cp68")
     
@@ -596,7 +596,7 @@ if __name__ == "__main__":
 #              'MSR', 'MCH', 'TVB', 'TBD']
 
     ticker = ['GVR']
-    end_date = "2019-6-13"
+    end_date = "2019-5-31"
     start_date = "2018-4-6"
     analysis_trading(tickers = None, start = start_date , end = end_date, update = False,  source ="cp68", trade = 'Long')
 ####    
