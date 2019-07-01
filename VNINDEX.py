2a2719c4b6bbfa4bc0cae82d323dda2a796c52db
ngtrungson <sonngtrung@gmail.com>
2019-07-01 18:40:19 +0700

    update database 01.07

--- VNINDEX.py
+++ VNINDEX.py
@@ -232,7 +232,7 @@ def analysis_trading(tickers, start, end, update = False, source = "cp68", trade
 #            ninja_trading(ticker, start, end, realtime = update, source = source)
 #            hedgefund_trading(ticker, start, end, realtime = update, source = source)
 #            hung_canslim(ticker, start, end, realtime = update, source = source, ndays = 5, typetrade = 'MarkM_tickers')#           
-            hung_canslim(ticker, start, end, realtime = update, source = source, ndays = 15, typetrade = trade)
+            hung_canslim(ticker, start, end, realtime = update, source = source, ndays = 1, typetrade = trade)
 #            hung_canslim(ticker, start, end, realtime = update, source = source, ndays = 3, typetrade = 'Short')
 #            mean_reversion(ticker, start, end, realtime = update, source = source)
 #            bollinger_bands(ticker, start, end, realtime = update, source = source)
@@ -626,7 +626,7 @@ if __name__ == "__main__":
 #              'MSR', 'MCH', 'TVB', 'TBD']
 
     ticker = ['NAF','DBC','ACV','NDN','HNG']
-    end_date = "2019-6-28"
+    end_date = "2019-7-1"
     start_date = "2018-4-6"
     analysis_trading(tickers = None, start = start_date , end = end_date, update = False,  source ="cp68", trade = 'Long')
 ####    
