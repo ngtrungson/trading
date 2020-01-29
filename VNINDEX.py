d5841711e4f5a910228b4cbe798e1d7dcc7fa328
ngtrungson <36395701+ngtrungson@users.noreply.github.com>
2020-01-30 06:43:04 +0700

    update database 22.01

--- VNINDEX.py
+++ VNINDEX.py
@@ -627,7 +627,7 @@ if __name__ == "__main__":
 #              'MSR', 'MCH', 'TVB', 'TBD']
 
     ticker = ['CTR','VGI','BWE','TDM']
-    end_date = "2020-1-3"
+    end_date = "2020-1-22"
     start_date = "2018-4-6"
 #    analysis_trading(tickers = None, start = start_date , end = end_date, update = False,  source ="cp68", trade = 'Long')
 ####    
