e4b40fac79cf46067aa3148b7eaec0507ce3c972
ngtrungson <36395701+ngtrungson@users.noreply.github.com>
2020-09-11 21:38:17 +0700

    update database 11.9

--- VNINDEX.py
+++ VNINDEX.py
@@ -635,7 +635,7 @@ if __name__ == "__main__":
     sys.stdout=open("logging.txt","w")
 #   
 ##    
-    # symbols = get_csv_data(source = "cp68")
+    # symbols = get_csv_data(source = "ssi")
 #    symbols = get_csv_data()
 #    symbols = get_stocks_highcpm(download = False, source ="cp68")
     
@@ -657,7 +657,7 @@ if __name__ == "__main__":
 #              'MSR', 'MCH', 'TVB', 'TBD']
 
     ticker = ['CTR','VGI','BWE','TDM']
-    end_date = "2020-9-10"
+    end_date = "2020-9-11"
     start_date = "2019-4-6"
     ticker = 'CTD'
     # canslim = hung_canslim(ticker, start_date, end_date, realtime = False,  source ="cp68", ndays = 1, typetrade = 'SidewayBreakout') 
@@ -677,7 +677,7 @@ if __name__ == "__main__":
     
     
     #CHON CO PHIEU CO DIEM MUA BUNG NO KHOI LUONG
-    analysis_trading(tickers = None, start = start_date , end = end_date, update = False, nbdays = 5, source ="cp68", trade = 'LongShortTrend')
+    analysis_trading(tickers = None, start = start_date , end = end_date, update = False, nbdays = 2, source ="cp68", trade = 'LongShortTrend')
     
 ###    
     
