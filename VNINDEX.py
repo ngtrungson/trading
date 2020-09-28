95f433ac62c477d3901f279bc29d11d8362893e6
ngtrungson <36395701+ngtrungson@users.noreply.github.com>
2020-09-28 23:29:08 +0700

    update database 28.9

--- VNINDEX.py
+++ VNINDEX.py
@@ -635,7 +635,7 @@ if __name__ == "__main__":
     sys.stdout=open("logging.txt","w")
 #   
 #    
-    # symbols = get_csv_data(source = "ssi")
+    symbols = get_csv_data(source = "cp68")
 #    symbols = get_csv_data()
 #    symbols = get_stocks_highcpm(download = False, source ="cp68")
     
@@ -677,7 +677,7 @@ if __name__ == "__main__":
     
     
     #CHON CO PHIEU CO DIEM MUA BUNG NO KHOI LUONG
-    analysis_trading(tickers = None, start = start_date , end = end_date, update = False, nbdays = 5, source ="cp68", trade = 'LongShortTrend')
+    # analysis_trading(tickers = None, start = start_date , end = end_date, update = False, nbdays = 5, source ="cp68", trade = 'LongShortTrend')
     
 ###    
     
