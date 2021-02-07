1c1217034325132ad7451aced092a56db1a3ed28
ngtrungson <36395701+ngtrungson@users.noreply.github.com>
2021-02-08 06:55:17 +0700

    update database 05.2

--- VNINDEX.py
+++ VNINDEX.py
@@ -658,7 +658,7 @@ if __name__ == "__main__":
 #              'MSR', 'MCH', 'TVB', 'TBD']
 
     ticker = ['CTR','VGI','BWE','TDM']
-    end_date = "2021-2-2"
+    end_date = "2021-2-5"
     start_date = "2019-4-6"
     ticker = ['^VNINDEX']
     # canslim = hung_canslim(ticker, start_date, end_date, realtime = False,  source ="cp68", ndays = 1, typetrade = 'SidewayBreakout') 
