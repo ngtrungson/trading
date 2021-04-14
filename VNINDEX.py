30ca0220cc325c566fc6134d5c2063006579de5f
ngtrungson <36395701+ngtrungson@users.noreply.github.com>
2021-04-14 22:30:08 +0700

    update database 14.4

--- VNINDEX.py
+++ VNINDEX.py
@@ -658,7 +658,7 @@ if __name__ == "__main__":
     
 #   
 #    
-    # symbols = get_csv_data(source = "ssi")
+    # symbols = get_csv_data(source = "cp68")
 #    symbols = get_csv_data()
 #    symbols = get_stocks_highcpm(download = False, source ="cp68")
     
@@ -680,7 +680,7 @@ if __name__ == "__main__":
 #              'MSR', 'MCH', 'TVB', 'TBD']
 
     ticker = ['CTR','VGI','BWE','TDM']
-    end_date = "2021-4-13"
+    end_date = "2021-4-14"
     start_date = "2019-4-6"
     ticker = 'DGC'
     # canslim = hung_canslim(ticker, start_date, end_date, realtime = False,  source ="cp68", ndays = 1, typetrade = 'EarlyBreakout') 
