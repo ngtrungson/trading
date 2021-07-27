e92122874e53012e812971e5002f18d69356028c
Trung-Son.Nguyen <sonngtrung@gmail.com>
2021-07-27 17:16:58 +0700

    update database 27.7

--- VNINDEX.py
+++ VNINDEX.py
@@ -658,7 +658,7 @@ if __name__ == "__main__":
     
 #   
 #    
-    # symbols = get_csv_data(source = "ssi")
+    # symbols = get_csv_data(source = "cp68")
 #    symbols = get_csv_data()
 #    symbols = get_stocks_highcpm(download = False, source ="cp68")
     
@@ -680,7 +680,7 @@ if __name__ == "__main__":
 #              'MSR', 'MCH', 'TVB', 'TBD']
 
     ticker = ['CTR','VGI','BWE','TDM']
-    end_date = "2021-7-26"
+    end_date = "2021-7-27"
     start_date = "2019-4-6"
     ticker = 'DGC'
     # canslim = hung_canslim(ticker, start_date, end_date, realtime = False,  source ="cp68", ndays = 1, typetrade = 'EarlyBreakout') 
@@ -705,7 +705,7 @@ if __name__ == "__main__":
     trade_type = ['EarlySignal','Bottom','SidewayBreakout']
     idx = 0 # EarlySignal
     realtime = True
-    datasource = "ssi"
+    datasource = "cp68"
     t1 = 9*60 + 20   
     t2 = 11*60 + 30   
     t3 = 13*60 + 0  
