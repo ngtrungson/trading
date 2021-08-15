bf038917265facc1513e0403383967ff30e5f971
ngtrungson <36395701+ngtrungson@users.noreply.github.com>
2021-08-15 23:02:15 +0700

    update database 13.8

--- VNINDEX.py
+++ VNINDEX.py
@@ -684,7 +684,7 @@ if __name__ == "__main__":
 #              'MSR', 'MCH', 'TVB', 'TBD']
 
     ticker = ['CTR','VGI','BWE','TDM']
-    end_date = "2021-8-12"
+    end_date = "2021-8-13"
     start_date = "2019-4-6"
     ticker = 'DGC'
     # canslim = hung_canslim(ticker, start_date, end_date, realtime = False,  source ="cp68", ndays = 1, typetrade = 'EarlyBreakout') 
@@ -708,13 +708,13 @@ if __name__ == "__main__":
     t0 = time.time()
     trade_type = ['EarlySignal','Bottom','SidewayBreakout']
     idx = 0 # EarlySignal
-    realtime = not True
+    realtime = True
     datasource = "cp68"
     t1 = 9*60 + 20   
     t2 = 11*60 + 30   
     t3 = 13*60 + 0  
     t4 = 14*60 + 45
-    trading = True
+    trading = not True
     while trading:  
         # clear_output(wait=True)
         trade_time = datetime.now()
