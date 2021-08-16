5151a997882fa3d1ce5713811d104fac06254964
Trung-Son.Nguyen <sonngtrung@gmail.com>
2021-08-16 16:38:46 +0700

    update database 16.8

--- VNINDEX.py
+++ VNINDEX.py
@@ -684,7 +684,7 @@ if __name__ == "__main__":
 #              'MSR', 'MCH', 'TVB', 'TBD']
 
     ticker = ['CTR','VGI','BWE','TDM']
-    end_date = "2021-8-13"
+    end_date = "2021-8-14"
     start_date = "2019-4-6"
     ticker = 'DGC'
     # canslim = hung_canslim(ticker, start_date, end_date, realtime = False,  source ="cp68", ndays = 1, typetrade = 'EarlyBreakout') 
