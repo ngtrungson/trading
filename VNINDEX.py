c7c838af87d40d944518bf698a051e5bf157d60e
Trung-Son.Nguyen <sonngtrung@gmail.com>
2021-09-14 16:52:57 +0700

    update database 14.9

--- VNINDEX.py
+++ VNINDEX.py
@@ -685,7 +685,7 @@ if __name__ == "__main__":
 #              'MSR', 'MCH', 'TVB', 'TBD']
 
     ticker = ['CTR','VGI','BWE','TDM']
-    end_date = "2021-9-8"
+    end_date = "2021-9-13"
     start_date = "2019-4-6"
     # stock_all, market_all = analysis_stocks(start_date = start_date, end_date = end_date, realtime = False, source = 'cp68')
     
